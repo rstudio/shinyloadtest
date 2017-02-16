a0d48203116b78b7787f4db1b5b509f726228926
Sean Lopp <sean@rstudio.com>
2017-02-15 21:42:10 -0700

    Adding option to stagger connections by adding a pause. Pause time is distributed uniformally

--- R/test-load.R
+++ R/test-load.R
@@ -2,9 +2,13 @@
 #'
 #' @param testFile The file containing a test script to run. Test script can be
 #'   generated from \code{recordTest(load_mode = TRUE)}.
+#' @param url Web address of the deployed Shiny application.
 #' @param numConcurrent Number of concurrent connections to simulate.
 #' @param numTotal Total number of connections to simulation.
-#' @param url Web address of the deployed Shiny application.
+#' @param stagger  Concurrent connections are staggered by pausing for a random,
+#'   uniformally distributed period of time before starting the connection. This
+#'   parameter is the maximum amount of time (sec) to wait. A value of 0 means
+#'   that no staggering will occur.
 #'
 #' @details This function simulates load against a deployed Shiny app. The
 #'   function creates a cluster of workers using the parallel function
@@ -17,14 +21,21 @@
 #'    will be recycled to complete remaining tests.The phantomJS process is
 #'   reused, but a new browser session is started for each test.
 #'
+#' @return If all of the child processes complete successfully this function
+#'   returns a dataframe containing timing and event information. If not all of
+#'   the child processes suceed, a list is returned containing timing for
+#'   successful tests and error messages for failed tests.
+#'
 #' @importFrom foreach %dopar% foreach
 #' @importFrom assertthat assert_that
 #'
 #' @export
 loadTest <- function(testFile = "./tests/myloadtest.R",
+                     url = NULL,
                      numConcurrent = 4,
                      numTotal = numConcurrent,
-                     url = NULL) {
+                     stagger = 5
+                     ) {
 
   assert_that(file.exists(testFile))
 
@@ -53,7 +64,7 @@ loadTest <- function(testFile = "./tests/myloadtest.R",
 
   ## Loop Through Connections
 
-  message(paste0('====== Begining Load Test ======'))
+  message(paste0('====== Beginning Load Test ======'))
 
   results <- foreach::foreach(i = 1:numTotal,
     .errorhandling = 'pass',
@@ -61,6 +72,8 @@ loadTest <- function(testFile = "./tests/myloadtest.R",
       withr::with_options(
         list(target.url = url, connection.id = i), {
           env <- new.env(parent = .GlobalEnv)
+
+          Sys.sleep(runif(1, 0, stagger))
 
           ## The test file will return a data frame
           ## with the relevant timing information.
