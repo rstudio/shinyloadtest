68d609945a1fe44a71add73c9c1d279b2ad408f1
Alan Dipert <alan@dipert.org>
2018-02-23 09:57:24 -0800

    Package-ized Winston's code, brought in logging

--- R/shinyRecorder.R
+++ R/shinyRecorder.R
@@ -1,75 +1,84 @@
+req_rook_to_curl <- function(req, domain, port) {
+  # browser()
+  # Rename headers. Example: HTTP_CACHE_CONTROL => Cache-Control
+  r <- as.list(req)
+
+  # Log request headers
+  logging::logdebug("== Original ==\n")
+  logging::logdebug(capture.output(print(str(r))), sep = "\n")
+
+  r <- r[grepl("^HTTP_", names(r))]
+  nms <- names(r)
+  nms <- sub("^HTTP_", "", nms)
+  nms <- tolower(nms)
+  nms <- gsub("_", "-", nms, fixed = TRUE)
+  nms <- gsub("\\b([a-z])", "\\U\\1", nms, perl = TRUE)
+  names(r) <- nms
+
+  # Overwrite host field
+  if (port == 80) {
+    r$Host <- domain
+  } else {
+    r$Host <- paste0(domain, ":", port)
+  }
+
+  # Log modified request headers
+  logging::logdebug("== Modified ==\n")
+  logging::logdebug(capture.output(print(str(r))), sep = "\n")
+  r
+}
 
 
+resp_httr_to_rook <- function(resp) {
+  # browser()
+  status <- as.integer(sub("^HTTP\\S+ (\\d+).*", "\\1", curl::parse_headers(resp$headers)[1]))
+  list(
+    status = status,
+    headers = curl::parse_headers_list(resp$headers),
+    body = resp$content
+  )
+}
 
-# 
-# # library(curl)
-# # library(httpuv)
-# # library(websocketClient)
-# 
-# 
-# 
-# req_rook_to_curl <- function(req, host) {
-#   # browser()
-#   # Rename headers. Example: HTTP_CACHE_CONTROL => Cache-Control
-#   r <- as.list(req)
-#   
-#   # Uncomment to print out request headers
-#   #cat("== Original ==\n")
-#   #cat(capture.output(print(str(r))), sep = "\n")
-#   
-#   r <- r[grepl("^HTTP_", names(r))]
-#   nms <- names(r)
-#   nms <- sub("^HTTP_", "", nms)
-#   nms <- tolower(nms)
-#   nms <- gsub("_", "-", nms, fixed = TRUE)
-#   nms <- gsub("\\b([a-z])", "\\U\\1", nms, perl = TRUE)
-#   names(r) <- nms
-#   
-#   # Overwrite host field
-#   r$Host <- host
-#   # Uncomment to print out modified request headers
-#   #cat("== Modified ==\n")
-#   #cat(capture.output(print(str(r))), sep = "\n")
-#   r
-# }
-# 
-# 
-# resp_httr_to_rook <- function(resp) {
-#   # browser()
-#   status <- as.integer(sub("^HTTP\\S+ (\\d+).*", "\\1", curl::parse_headers(resp$headers)[1]))
-#   list(
-#     status = status,
-#     headers = parse_headers_list(resp$headers),
-#     body = resp$content
-#   )
-# }
-# 
-# 
-# s <- startServer("0.0.0.0", 5002,
-#   list(
-#     call = function(req) {
-#       host <- "localhost:3923"
-#       
-#       req_curl <- req_rook_to_curl(req, host)
-#       h <- new_handle()
-#       do.call(handle_setheaders, c(h, req_curl))
-#       
-#       resp_curl <- curl_fetch_memory(paste0("http://", host, req$PATH_INFO), handle = h)
-#       
-#       resp_httr_to_rook(resp_curl)
-#     },
-#     onWSOpen = function(clientWS) {
-#       serverWS <- WebsocketClient$new("ws://localhost:3923", onMessage = function(msgFromServer) {
-#         cat("Got message from server: ", msgFromServer, "\n")
-#         clientWS$send(msgFromServer)
-#       })
-#       clientWS$onMessage(function (isBinary, msgFromClient) {
-#         cat("Got message from client: ", msgFromClient, "\n")
-#         serverWS$send(msgFromClient)
-#       })
-#     }
-#   )
-# )
-# 
-# # Stop the server
-# # stopServer(s)
+`%OR%` <- function(x, y) {
+  if (is.null(x) || isTRUE(is.na(x)))
+    y
+  else
+    x
+}
+
+#' Title
+#'
+#' @param targetAppUrl
+#' @param listenPort
+#'
+#' @return
+#' @export
+#'
+#' @examples
+recordSession <- function(targetAppUrl, listenPort = 8600) {
+  parsedUrl <- urltools::url_parse(targetAppUrl)
+  targetAppPort <- parsedUrl$port %OR% 80
+  httpuv::startServer("0.0.0.0", listenPort,
+    list(
+      call = function(req) {
+        req_curl <- req_rook_to_curl(req, parsedUrl$domain, targetAppPort)
+        h <- curl::new_handle()
+        do.call(curl::handle_setheaders, c(h, req_curl))
+        httpUrl <- paste0("http://", parsedUrl$domain, ":", targetAppPort, req$PATH_INFO)
+        resp_curl <- curl::curl_fetch_memory(httpUrl, handle = h)
+        resp_httr_to_rook(resp_curl)
+      },
+      onWSOpen = function(clientWS) {
+        wsUrl <- paste0("ws://", parsedUrl$domain, ":", targetAppPort)
+        serverWS <- websocketClient::WebsocketClient$new(wsUrl, onMessage = function(msgFromServer) {
+          cat("Got message from server: ", msgFromServer, "\n")
+          clientWS$send(msgFromServer)
+        })
+        clientWS$onMessage(function (isBinary, msgFromClient) {
+          cat("Got message from client: ", msgFromClient, "\n")
+          serverWS$send(msgFromClient)
+        })
+      }
+    )
+  )
+}
