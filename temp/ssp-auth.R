a8935481a2fc3f5364e0e05230f71e2e1cb12841
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2018-04-16 16:40:36 +0100

    start post req

--- temp/ssp-auth.R
+++ temp/ssp-auth.R
@@ -2,23 +2,20 @@
 install.packages("xml2")
 library(xml2)
 
+resLoginForm <- httr::GET("http://localhost:3838/sample-apps/hello/")
+login <- "/Users/barbaraborges/Desktop/login.html"
+login_html <- xml2::read_html(readChar(login, file.info(login)$size))
+
 getHiddenInputs <- function(login_html) {
-  inputs <- xml_find_all(login_html, "//input[@type='hidden']")
-  attrs <- xml_attrs(inputs)
-  Map(function(input) c(input["name"], input["value"]), attrs)
+  inputs <- xml2::xml_find_all(login_html, "//input[@type='hidden']")
+  attrs <- xml2::xml_attrs(inputs)
+  lst <- Map(function(input) c(input["name"], input["value"]), attrs)
+  df <- Reduce(rbind, Map(function(v) data.frame(as.list(v)), lst))
+  df$name <- as.character(df$name)
+  df$value <- as.character(df$value)
+  df
 }
 
-
-login <- "/Users/barbaraborges/Desktop/login.html"
-login_html <- readChar(login, file.info(login)$size) %>% read_html()
-
-login_html %>%
-  xml_find_all("//input[@type='hidden']")
-
-attrs <- login_xml %>%
-  xml_attrs() %>%
-  Map(function(x) c(x["name"], x["value"]), .)
-
-getLoginForm <- function(url = "http://localhost:3838/sample-apps/hello/") {
-  res <- httr::GET(url)
+sendPostReq <- function() {
+httr::POST()
 }
