a766aea18634334e0881b7805d6a3f2ce93208a7
Alan Dipert <alan@dipert.org>
2018-06-20 11:52:39 -0700

    WIP url builder fixes

--- R/url.R
+++ R/url.R
@@ -3,15 +3,6 @@ URLBuilder <- R6::R6Class("URLBuilder",
     initialize = function(str) {
       # TODO parse str if non-missing and non-empty
     },
-    clone = function() {
-      copy <- URLBuilder$new()
-      copy$scheme <- self$scheme
-      copy$host <- self$host
-      copy$port <- self$port
-      copy$paths <- self$paths
-      fragment <- self$fragment
-      copy
-    },
     setScheme = function(scheme) {
       if (!scheme %in% c("http", "https", "ws", "wss"))
         error("Unknown URL scheme: ", scheme)
@@ -23,22 +14,22 @@ URLBuilder <- R6::R6Class("URLBuilder",
       copy <- self$clone()
       copy$host <- host
       copy
-    }
+    },
     setPort = function(port) {
       if (!is.numeric(port) || port < 0 || port > 65535)
         error("Invalid port: ", port)
       copy <- self$clone()
       copy$port <- port
       copy
-    }
+    },
     appendPaths = function(paths, raw = FALSE) {
       # TODO assert paths is a character vector
       # TODO url encode on the way in if raw is TRUE
-    }
+    },
     build = function() {
       # TODO construct valid url string and return
       # TODO error if any required pieces are missing
-    }
+    },
     scheme = "http",
     host = NULL,
     port = NULL,
