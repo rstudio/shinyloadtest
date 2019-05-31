a66836b87342009123b1e1e6f810dd4579731266
Alan Dipert <alan@dipert.org>
2019-05-31 07:34:18 -0700

    WIP enum

---
+++ R/enum.R
@@ -0,0 +1,29 @@
+seteq <- function(v1, v2) {
+  stopifnot(is.vector(v1), is.vector(v2), typeof(v1) == typeof(v2))
+  if (length(v1) != length(v2)) return(FALSE)
+  for (x in v1) if (!(x %in% v2)) return(FALSE)
+  TRUE
+}
+
+enum <- function(...) {
+  fields <- as.character(eval(substitute(alist(...))))
+  stopifnot(all(grepl("^[[:upper:]]+$", fields)))
+  stopifnot(seteq(fields, unique(fields)))
+  structure(list(fields = fields), class = "shinyloadtest.enum")
+}
+
+`$.shinyloadtest.enum` <- function(x, fld) {
+  m <- match(fld, x[["fields"]])
+  if (is.na(m)) stop(paste0("Unknown field '", fld, "' of enum"))
+  m
+}
+
+types <- enum(FOO, BAR, BAZ)
+types$FOO
+types$LOL # Error in `$.shinyloadtest.enum`(types, LOL) : Unknown field 'LOL' of enum
+
+enum_case <- function(enum, ...) {
+  stopifnot(class(enum) == "shinyloadtest.enum")
+  # TODO dispatch on clauses, ensure exhaustiveness
+}
+
