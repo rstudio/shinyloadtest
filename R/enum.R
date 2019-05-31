5d670c52983fe3157c7b1e467abb1bf4246b405d
Alan Dipert <alan@dipert.org>
2019-05-31 12:21:49 -0700

    Finish enums prototype

--- R/enum.R
+++ R/enum.R
@@ -1,36 +1,89 @@
-seteq <- function(v1, v2) {
-  stopifnot(is.vector(v1), is.vector(v2), typeof(v1) == typeof(v2))
-  if (length(v1) != length(v2)) return(FALSE)
-  for (x in v1) if (!(x %in% v2)) return(FALSE)
-  TRUE
+enum_field <- function(name) {
+  env <- as.environment(list(
+    name = name,
+    parent = NULL,
+    set_parent = function(parent) (env$parent <- parent)
+  ))
+  structure(env, class = "shinyloadtest.enum_field")
+}
+
+`==.shinyloadtest.enum_field` <- function(f1, f2) {
+  if (!(class(f2) == "shinyloadtest.enum_field")) return(FALSE)
+  stopifnot(!is.null(f1$parent) && !is.null(f2$parent))
+  (f1$parent == f2$parent) && (f1$name == f2$name)
 }
 
 enum <- function(...) {
+  # TODO Use rlang for ... here
   fields <- as.character(eval(substitute(alist(...))))
   stopifnot(all(grepl("^[[:upper:]]+$", fields)))
-  stopifnot(seteq(fields, unique(fields)))
-  structure(list(fields = fields), class = "shinyloadtest.enum")
+  stopifnot(base::setequal(fields, unique(fields)))
+  enum_fields <- lapply(fields, function(f) enum_field(f))
+  self <- structure(list(fields = enum_fields), class = "shinyloadtest.enum")
+  for (field in enum_fields) field$set_parent(self)
+  self
 }
+
+enum_fieldnames <- function(enum) {
+  vapply(enum[["fields"]], function(field) field$name, character(1))
+}
+
+`==.shinyloadtest.enum` <- identical
 
 `$.shinyloadtest.enum` <- function(x, fld) {
-  m <- match(fld, x[["fields"]])
-  if (is.na(m)) stop(paste0("Unknown field '", fld, "' of enum"))
-  m
+  for (f in x[["fields"]]) if (f$name == fld) return(f)
+  stop(paste0("Unknown field '", fld, "' of enum"))
 }
 
-types <- enum(FOO, BAR, BAZ)
-types$FOO
-types$LOL # Error in `$.shinyloadtest.enum`(types, LOL) : Unknown field 'LOL' of enum
-
-enum_case <- function(enum, ...) {
-  stopifnot(class(enum) == "shinyloadtest.enum")
-  # TODO check exhaustiveness, eval matching field handler
+enum_case <- function(field, ...) {
+  stopifnot(class(field) == "shinyloadtest.enum_field")
+  cases <- rlang::enexprs(..., .named = TRUE, .homonyms = "error")
+  unknown_fields <- base::setdiff(names(cases), enum_fieldnames(field$parent))
+  if (length(unknown_fields)) stop(paste("Unknown field names", unknown_fields))
+  missing_fields <- base::setdiff(enum_fieldnames(field$parent), names(cases))
+  if (length(missing_fields)) stop(paste("Missing field names", missing_fields))
+  rlang::eval_tidy(cases[[field$name]])
 }
 
-# enum_case(types,
+# The idea here is we have a class representing a set of named, distinct, uppercase fields:
+#
+# Frobs <- enum(FOO, BAR, BAZ)
+#
+# Partial matching of a field, or referring to an unknown field, results in an error:
+#
+# Frobs$FO
+# Error in `$.shinyloadtest.enum`(types, FO) : Unknown field 'FO' of enum
+#
+# Frobs$LOL
+# Error in `$.shinyloadtest.enum`(types, LOL) : Unknown field 'LOL' of enum
+#
+# A conditional construct, `enum_case`, is provided to dispatch
+# on a field and to perform exhaustiveness checking at runtime:
+#
+# some_field <- frobs$FOO
+# enum_case(some_field,
 #   FOO = "foo",
 #   BAR = "bar"
 # )
 #
-# Error: field BAZ of enum 'types' is not handled
-
+# Error in enum_case(some_field, FOO = "foo", BAR = "bar") :
+#   Missing field names BAZ
+#
+# An error message is also produced if a field is mentioned that doesn't exist
+# in the enum:
+#
+# some_field <- frobs$FOO
+# enum_case(some_field,
+#   FOO = "foo",
+#   BAR = "bar",
+#   BAZ = "baz",
+#   QUX = "qux"
+# )
+#
+# Error in enum_case(some_field, FOO = "foo", BAR = "bar", BAZ = "baz",  :
+#  Unknown field names QUX
+#
+# Since the exhaustiveness checking is at runtime, the use of enums doesn't provide
+# any safety guarantees that couldn't be provided by e.g. assertions. However,
+# if use of an enum-like construct is desirable, then using this mechanism
+# provides a degree of run-time error checking without explicit assertions.
