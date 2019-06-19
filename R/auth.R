rstudio/shinyloadtest

commit 7572a292e21d63a73dbf6de403e2537b72f65240

Embed target server type (#107)
Corresponds to rstudio/shinycannon#36

R/auth.R
@@ -16,27 +16,10 @@ pasteParams <- function(df, collapse) {
   }
 }
 
-# Returns string "unknown", "ssp", "rsc"
-# TODO "shinyapps.io"
-servedBy <- function(appUrl) {
-  h <- curl::new_handle()
-  curl::handle_setopt(h, ssl_verifyhost = 0, ssl_verifypeer = 0)
-  resp <- curl::curl_fetch_memory(appUrl, handle = h)
-  df <- curl::handle_cookies(h)
-  headers <- curl::parse_headers_list(resp$headers)
-  if (nrow(df[which(df$name == "SSP-XSRF"),]) == 1
-    || isTRUE(headers[["x-powered-by"]] %in% c("Express", "Shiny Server", "Shiny Server Pro"))) {
-    return("ssp")
-  } else if (nrow(df[which(df$name == "rscid"),]) == 1) {
-    return("rsc")
-  }
-  "unknown"
-}
-
 isProtected <- function(appUrl) {
   h <- curl::new_handle()
   curl::handle_setopt(h, ssl_verifyhost = 0, ssl_verifypeer = 0)
-  resp <- curl::curl_fetch_memory(appUrl, handle = h)
+  resp <- curl::curl_fetch_memory(appUrl$build(), handle = h)
   # NOTE: Connect returns a 404 if the app exists but requires authentication.
   # So we don't have a way to distinguish between an appUrl that doesn't exist
   # and an app that's protected.
@@ -45,8 +28,8 @@ isProtected <- function(appUrl) {
 }
 
 loginUrlFor <- function(appUrl, appServer) {
-  if (appServer %in% c("rsc", "ssp")) {
-    URLBuilder$new(appUrl)$appendPaths("__login__")$build()
+  if (appServer %in% c(SERVER_TYPE$RSC, SERVER_TYPE$SSP)) {
+    appUrl$appendPaths("__login__")
   } else {
     stop(paste0("Unknown appServer:", appServer))
   }
@@ -62,37 +45,53 @@ handlePost <- function(handle, loginUrl, postfields, cookies, cookieName) {
     ssl_verifyhost = 0,
     ssl_verifypeer = 0
   )
-  curl::curl_fetch_memory(loginUrl, handle = handle)
+
+  resp <- curl::curl_fetch_memory(loginUrl$build(), handle = handle)
+  if (resp$status_code != 200) stop("Authentication failed")
+
   curl::handle_cookies(handle)[,c("name", "value")]
 }
 
 # Returns the cookies that should be attached to all subsequent HTTP requests,
 # including the initial websocket request. Currently implemented for RSC and
 # SSP.
-postLogin <- function(appUrl, username, password) {
+postLogin <- function(appUrl, appServer, username, password) {
 
-  appServer <- servedBy(appUrl)
   loginUrl <- loginUrlFor(appUrl, appServer)
 
   h <- curl::new_handle()
   curl::handle_setopt(h, ssl_verifyhost = 0, ssl_verifypeer = 0)
-  resp <- curl::curl_fetch_memory(appUrl, handle = h)
+  resp <- curl::curl_fetch_memory(appUrl$build(), handle = h)
   login_html <- xml2::read_html(rawToChar(resp$content))
   inputs <- rbind(getInputs(login_html, appServer), data.frame(
     name = c("username", "password"), value = c(username, password))
   )
   cookies <- curl::handle_cookies(h)[,c("name", "value")]
-  if (appServer == "rsc") {
-    handlePost(handle = curl::new_handle(), loginUrl = loginUrl,
-      postfields = paste0('{"username": "', username, '", "password": "', password, '"}'),
-      cookies = cookies, cookieName = "rsconnect"
-    )
-  } else if (appServer == "ssp") {
-    handlePost(handle = curl::new_handle(), loginUrl = loginUrl,
-      postfields = utils::URLencode(pasteParams(inputs, "&")),
-      cookies = cookies, cookieName = "session_state"
-    )
-  }
+
+  enum_case(appServer,
+    RSC = handlePost(
+            handle = curl::new_handle(),
+            loginUrl = loginUrl,
+            postfields = jsonlite::toJSON(
+              list(
+                username = username,
+                password = password
+              ),
+              auto_unbox = TRUE
+            ),
+            cookies = cookies,
+            cookieName = "rsconnect"
+          ),
+    SSP = handlePost(
+            handle = curl::new_handle(),
+            loginUrl = loginUrl,
+            postfields = utils::URLencode(pasteParams(inputs, "&")),
+            cookies = cookies,
+            cookieName = "session_state"
+          ),
+    SAI = stop("Logging in to shinyapps.io is unsupported"),
+    SHN = stop("Plain Shiny apps don't support authentication")
+  )
 }
 
 getApp <- function(appUrl, cookie) {

DESCRIPTION
@@ -1,50 +1,54 @@
 Package: shinyloadtest
 Type: Package
 Title: Load Test Shiny Applications
-Version: 1.0.0
+Version: 1.0.0.9000
 Authors@R: c(
             person("Alan", "Dipert", role = c("aut", "cre"), email = "alan@rstudio.com"),
             person("Barret", "Schloerke", role = c("aut"), email = "barret@rstudio.com"),
             person("Barbara", "Borges", role = c("aut")),
             person("RStudio", role = c("cph", "fnd"))
             )
-Description: shinyloadtest provides functions for recording user sessions,
-    playing those sessions back in parallel to simulate load, and analyzing
-    the results.
+Description: Facilities for recording Shiny application sessions and analyzing load test results.
 License: GPL-3
 Encoding: UTF-8
 LazyData: true
 BugReports: https://github.com/rstudio/shinyloadtest/issues
+Collate:
+  'enum.R'
+  'analysis.R'
+  'detect.R'
+  'auth.R'
+  'make_report.R'
+  'plotting.R'
+  'shiny-recorder.R'
+  'url.R'
+  'util.R'
+  'util-pipe.R'
 Imports:
-    websocket (>= 0.0.0.9001),
-    curl,
-    httpuv,
     R6,
-    jsonlite,
-    stringr,
-    rlang (>= 0.1.2),
-    xml2,
+    curl,
+    dplyr (>= 0.8.0.1),
     getPass,
     ggplot2,
-    tibble,
-    dplyr (>= 0.8.0.1),
-    readr,
-    magrittr,
-    viridis,
-    shiny,
     glue,
-    RColorBrewer,
-    lubridate,
-    DT,
-    flexdashboard,
-    rmarkdown,
-    progress,
-    scales,
-    htmltools,
     gtable,
-    svglite
+    htmltools,
+    httpuv (>= 1.5.1.9000),
+    jsonlite,
+    lubridate,
+    magrittr,
+    progress,
+    readr,
+    rlang (>= 0.1.2),
+    rmarkdown,
+    scales,
+    stringr,
+    tibble,
+    websocket,
+    xml2
 Remotes:
-  rstudio/websocket
+  rstudio/websocket,
+  rstudio/httpuv@8492695
 RoxygenNote: 6.1.1
 Suggests:
     knitr,

NAMESPACE
@@ -1,15 +1,7 @@
 # Generated by roxygen2: do not edit by hand
 
 export("%>%")
-export(enexpr)
-export(enquo)
-export(ensym)
-export(expr)
-export(exprs)
 export(load_runs)
-export(quo)
-export(quo_name)
-export(quos)
 export(record_session)
 export(shinyloadtest_report)
 export(slt_hist_loadtimes)
@@ -22,21 +14,6 @@ export(slt_time_concurrency)
 export(slt_user)
 export(slt_waterfall)
 export(slt_websocket_latency)
-export(sym)
-export(syms)
 import(dplyr)
 import(ggplot2)
 importFrom(magrittr,"%>%")
-importFrom(rlang,.data)
-importFrom(rlang,UQ)
-importFrom(rlang,UQS)
-importFrom(rlang,enexpr)
-importFrom(rlang,enquo)
-importFrom(rlang,ensym)
-importFrom(rlang,expr)
-importFrom(rlang,exprs)
-importFrom(rlang,quo)
-importFrom(rlang,quo_name)
-importFrom(rlang,quos)
-importFrom(rlang,sym)
-importFrom(rlang,syms)

R/detect.R
@@ -1,0 +1,50 @@
+SERVER_TYPE <- enum(
+  RSC,
+  SSP,
+  SAI,
+  SHN
+)
+
+format_server_type <- function(field) {
+  enum_case(field,
+    RSC = "RStudio Server Connect",
+    SSP = "Shiny Server or Shiny Server Pro",
+    SAI = "shinyapps.io",
+    SHN = "R/Shiny"
+  )
+}
+
+servedBy <- function(appUrl) {
+
+  if (grepl("\\.shinyapps\\.io$", appUrl$host)) return(SERVER_TYPE$SAI)
+
+  h <- curl::new_handle()
+  curl::handle_setopt(
+    h,
+    ssl_verifyhost = FALSE,
+    ssl_verifypeer = FALSE,
+    followlocation = TRUE
+  )
+  resp <- curl::curl_fetch_memory(appUrl$build(), handle = h)
+  df <- curl::handle_cookies(h)
+  headers <- curl::parse_headers_list(resp$headers)
+
+  hasShinyJS <- tryCatch({
+    html <- xml2::read_html(rawToChar(resp$content))
+    scripts <- xml2::xml_find_all(html, "/html/head/script")
+    srcs <- unlist(lapply(scripts, function(script) xml2::xml_attr(script, "src")))
+    srcs <- srcs[!is.na(srcs)]
+    any(grepl("/shiny(\\.min)?\\.js$", srcs))
+  }, error = function(e) FALSE)
+
+  if (nrow(df[df$name == "SSP-XSRF",]) == 1
+    || isTRUE(headers[["x-powered-by"]] %in% c("Express", "Shiny Server", "Shiny Server Pro"))) {
+    return(SERVER_TYPE$SSP)
+  } else if (nrow(df[which(df$name == "rscid"),]) == 1) {
+    return(SERVER_TYPE$RSC)
+  } else if (hasShinyJS) {
+    return(SERVER_TYPE$SHN)
+  } else {
+    stop(paste("Target URL", appUrl$build(), "does not appear to be a Shiny application."))
+  }
+}

R/enum.R
@@ -1,0 +1,104 @@
+enum_value <- function(x, enum_id, all_val) {
+  structure(
+    x,
+    enum_id = enum_id,
+    all_val = all_val,
+    class = "shinyloadtest_enum_value"
+  )
+}
+
+`==.shinyloadtest_enum_value` <- function(x, y) {
+  if (class(y) != "shinyloadtest_enum_value") return(FALSE)
+  identical(x, y) && (attr(x, "enum_id") == attr(y, "enum_id"))
+}
+
+enum_counter <- local({
+  n <- 0L
+  function() (n <<- n + 1L)
+})
+
+enum <- function(...) {
+  val_sym <- rlang::ensyms(...)
+  val_str <- vapply(val_sym, as.character, character(1))
+  structure(
+    stats::setNames(lapply(val_str, enum_value, enum_counter(), val_str), val_str),
+    class = "shinyloadtest_enum"
+  )
+}
+
+`$.shinyloadtest_enum` <- function(x, i) {
+  if (!(i %in% names(x))) stop("Unknown enum value")
+  NextMethod()
+}
+`[[.shinyloadtest_enum` <- `$.shinyloadtest_enum`
+`==.shinyloadtest_enum` <- function(x, y) {
+  identical(x, y)
+}
+
+enum_case <- function(field, ...) {
+  stopifnot(inherits(field, "shinyloadtest_enum_value"))
+  cases <- rlang::enquos(..., .named = TRUE, .homonyms = "error")
+  all_val <- attr(field, "all_val")
+
+  unknown_values <- base::setdiff(names(cases), all_val)
+  if (length(unknown_values))
+    stop(paste("Unknown enum value", paste(unknown_values, collapse = ", ")), call. = FALSE)
+
+  missing_values <- base::setdiff(all_val, names(cases))
+  if (length(missing_values))
+    stop(paste("Missing enum value", paste(missing_values, collapse = ", ")), call. = FALSE)
+
+  rlang::eval_tidy(cases[[field]])
+}
+
+# The idea here is we have an object, Frobs, representing a set of named, distinct, values:
+#
+# Frobs <- enum(FOO, BAR, BAZ)
+#
+# Values are equal to themselves:
+#
+# Frobs$FOO == Frobs$FOO
+# [1] TRUE
+#
+# Values are not equal to values from other enums:
+#
+# Blobs <- enum(FOO, BAR, BAZ)
+#
+# Frobs$FOO == Blobs$FOO
+# [1] FALSE
+#
+# Partial matching of a value, or referring to an unknown value, produces an error:
+#
+# Frobs$FO
+# Error in `$.shinyloadtest_enum`(Frobs, FO) : Unknown enum value
+#
+# A conditional construct, `enum_case`, is provided to dispatch
+# on a value and to perform exhaustiveness checking of the other values from the enum
+# at runtime:
+#
+# some_field <- Frobs$FOO
+# enum_case(some_field,
+#   FOO = "foo",
+#   BAR = "bar"
+# )
+#
+# Error: Missing enum value BAZ
+#
+# An error message is also produced if a field is mentioned that doesn't exist
+# in the enum:
+#
+# some_val <- Frobs$FOO
+# enum_case(some_val, FOO = 1, BAR = 2, BAZ = 3, QUX = 3)
+# enum_case(some_field,
+#   FOO = "foo",
+#   BAR = "bar",
+#   BAZ = "baz",
+#   QUX = "qux"
+# )
+#
+# Error: Unknown enum value QUX
+#
+# Since the exhaustiveness checking is at runtime, the use of enums doesn't provide
+# any safety guarantees that couldn't be provided by e.g. assertions. However,
+# if use of an enum-like construct is desirable, then using this mechanism
+# provides a degree of run-time error checking without explicit assertions.

R/plotting.R
@@ -118,7 +118,7 @@ slt_time_boxplot <- function(df, labels = NULL) {
 
   if (!is.null(labels)) {
     labels <- enexpr(labels)
-    df <- df %>% filter(label %in% UQ(labels))
+    df <- df %>% filter(label %in% rlang::UQ(labels))
   }
 
   p <- df %>%
@@ -145,7 +145,7 @@ slt_time_concurrency <- function(df, labels = NULL) {
 
   if (!is.null(labels)) {
     labels <- enexpr(labels)
-    df <- df %>% filter(label %in% UQ(labels))
+    df <- df %>% filter(label %in% rlang::UQ(labels))
   }
 
   p <- df %>%
@@ -211,7 +211,6 @@ slt_waterfall <- function(df, limits = c(0, max(df$concurrency, na.rm = TRUE))) 
     # request_scale_guides() +
     scale_y_discrete(limits = y_limits) +
     geom_line(size = 0.5) +
-    # scale_color_viridis_c() +
     scale_colour_gradientn(colours = rev(c(run_fill_colors[c(3, 13, 6)], run_accent_colors[2])), limits = limits) +
     guides(
       color = guide_colorbar(order = 1),

R/shiny-recorder.R
@@ -105,11 +105,9 @@ parseMessage <- function(msg) {
 }
 
 replaceTokens <- function(str, tokens) {
-  if (length(tokens) > 0) {
+  if (length(tokens)) {
     stringr::str_replace_all(str, unlist(as.list(tokens)))
-  } else {
-    str
-  }
+  } else str
 }
 
 makeHTTPEvent_GET <- function(tokens, req, resp_curl, begin, end) {
@@ -184,29 +182,30 @@ shouldIgnoreGET <- function(path) {
   length(unlist(stringr::str_match_all(path, ignoreGET))) > 0
 }
 
-CLIENT_WS_STATE <- list(
-  UNOPENED = "UNOPENED",
-  OPEN = "OPEN",
-  CLOSED = "CLOSED"
-)
+CLIENT_WS_STATE <- enum(UNOPENED, OPEN, CLOSED)
 
 RecordingSession <- R6::R6Class("RecordingSession",
   public = list(
-    initialize = function(targetAppUrl, host, port, outputFileName, sessionCookies) {
+    initialize = function(targetAppUrl, host, port, outputFileName) {
       private$targetURL <- URLBuilder$new(targetAppUrl)
-      if (grepl("shinyapps.io$", private$targetURL$host)) {
+      private$targetType <- servedBy(private$targetURL)
+      if (private$targetType == SERVER_TYPE$SAI) {
         stop("Recording shinyapps.io apps is not supported")
       }
       private$localHost <- host
       private$localPort <- port
+
+      private$initializeSessionCookies()
+
       private$outputFileName <- outputFileName
       private$outputFile <- file(outputFileName, "w")
       header <- c(
-        paste0("# version: ", packageVersion("shinyloadtest")),
-        paste0("# target: ", targetAppUrl)
+        paste0("# version: 1"),
+        paste0("# target_url: ", targetAppUrl),
+        paste0("# target_type: ", format_server_type(private$targetType))
       )
       writeLines(header, private$outputFile)
-      private$sessionCookies <- sessionCookies
+      flush(private$outputFile)
       private$startServer()
     },
     stop = function() {
@@ -224,6 +223,7 @@ RecordingSession <- R6::R6Class("RecordingSession",
   ),
   private = list(
     targetURL = NULL,
+    targetType = NULL,
     localHost = NULL,
     localPort = NULL,
     localServer = NULL,
@@ -235,6 +235,17 @@ RecordingSession <- R6::R6Class("RecordingSession",
     writeEvent = function(evt) {
       writeLines(format(evt), private$outputFile)
       flush(private$outputFile)
+    },
+    initializeSessionCookies = function() {
+      cookies <- data.frame()
+      if (isProtected(private$targetURL)) {
+        username <- getPass::getPass("Enter your username: ")
+        if (is.null(username)) stop("Login aborted (username not provided)")
+        password <- getPass::getPass("Enter your password: ")
+        if (is.null(password)) stop("Login aborted (password not provided)")
+        cookies <- postLogin(private$targetURL, private$targetType, username, password)
+      }
+      private$sessionCookies <- cookies
     },
     mergeCookies = function(handle) {
       df <- curl::handle_cookies(handle)[,c("name", "value")]
@@ -462,18 +473,7 @@ RecordingSession <- R6::R6Class("RecordingSession",
 #' @export
 record_session <- function(target_app_url, host = "127.0.0.1", port = 8600,
   output_file = "recording.log", open_browser = TRUE) {
-    sessionCookies <- if (isProtected(target_app_url)) {
-      username <- getPass::getPass("Enter your username: ")
-      if (is.null(username)) {
-        return(invisible(FALSE))
-      }
-      password <- getPass::getPass("Enter your password: ")
-      if (is.null(password)) {
-        return(invisible(FALSE))
-      }
-      postLogin(target_app_url, username, password)
-    } else data.frame()
-    session <- RecordingSession$new(target_app_url, host, port, output_file, sessionCookies)
+    session <- RecordingSession$new(target_app_url, host, port, output_file)
     on.exit(session$stop())
     message("Listening on ", host, ":", port)
 

commit 645c959d0d597c20ce51fb6800753f045d7d4b4a

Fix authenticated SSP recording bug introduced by #107

R/auth.R
@@ -1,11 +1,11 @@
 
 getInputs <- function(html, server) {
-  if (server == "ssp") {
+  if (server == SERVER_TYPE$SSP) {
     inputs <- xml2::xml_find_all(html, "//input[@type='hidden']")
     attrs <- xml2::xml_attrs(inputs)
     attrs <- lapply(attrs, function(vec) c(name = vec[["name"]], value = vec[["value"]]))
     as.data.frame(do.call(rbind, attrs), stringsAsFactors = FALSE)
-  }
+  } else data.frame()
 }
 
 pasteParams <- function(df, collapse) {
@@ -47,7 +47,7 @@ handlePost <- function(handle, loginUrl, postfields, cookies, cookieName) {
   )
 
   resp <- curl::curl_fetch_memory(loginUrl$build(), handle = handle)
-  if (resp$status_code != 200) stop("Authentication failed")
+  if (!(resp$status_code %in% c(200, 302))) stop("Authentication failed")
 
   curl::handle_cookies(handle)[,c("name", "value")]
 }