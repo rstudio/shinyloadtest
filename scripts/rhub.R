f8d2a34ad7a92fcb17095f41e49c21971598da47
Barret Schloerke <schloerke@gmail.com>
2019-12-06 14:11:25 -0500

    Create rhub.R

---
+++ scripts/rhub.R
@@ -0,0 +1,12 @@
+
+
+email <- NULL
+# email <- "barret@rstudio.com"
+
+check_output <- devtools::check_rhub(email = email, interactive = FALSE)
+
+for (i in seq_len(nrow(check_output$urls()))) {
+  check_output$livelog(i)
+}
+
+check_output$cran_summary()
