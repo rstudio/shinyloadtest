2afc7b3dd60c393652dce09f19b510dc90483e97
Alan Dipert <alan@dipert.org>
2019-12-06 15:46:00 +0000

    Use skip_on_cran() properly

--- tests/testthat/test-report-gen.R
+++ tests/testthat/test-report-gen.R
@@ -2,9 +2,7 @@ context("report generation")
 
 test_that("reports are generated", {
   skip_on_cran()
-  expect_true({
-    html_file <- sprintf("%s.html", tempfile("report"))
-    shinyloadtest_report(slt_demo_data_1, output = html_file, open_browser = FALSE)
-    file.exists(html_file)
-  })
+  html_file <- sprintf("%s.html", tempfile("report"))
+  shinyloadtest_report(slt_demo_data_1, output = html_file, open_browser = FALSE)
+  expect_true(file.exists(html_file))
 })
