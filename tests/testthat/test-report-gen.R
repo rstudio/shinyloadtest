3a3595f58e1005145b47cda4c406269871e44f8b
Joe Cheng <joe@posit.co>
2024-07-05 14:32:40 -0700

    Add unit test; fix ggplot2 deprecation warnings

--- tests/testthat/test-report-gen.R
+++ tests/testthat/test-report-gen.R
@@ -6,3 +6,12 @@ test_that("reports are generated", {
   shinyloadtest_report(slt_demo_data_1, output = html_file, open_browser = FALSE)
   expect_true(file.exists(html_file))
 })
+
+test_that("legend extraction works", {
+  df <- data.frame(x = 1:3, y = 1:3, c = LETTERS[1:3], stringsAsFactors = FALSE)
+  p <- ggplot2::ggplot(df, ggplot2::aes(x = x, y = y, color = c)) +
+    ggplot2::geom_point() +
+    ggplot2::theme(legend.position = "bottom") +
+    ggplot2::labs(fill = "", color = "")
+  extract_legend(p)
+})
